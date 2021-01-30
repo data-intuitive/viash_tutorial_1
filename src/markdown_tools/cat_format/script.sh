a665d3ae8793a01fbb6d4ef3fe841efd758b2018
Toni Verbeiren <toni.verbeiren@gmail.com>
2021-01-30 18:24:01 +0100

    Update markdown_tools

--- src/markdown_tools/cat_format/script.sh
+++ src/markdown_tools/cat_format/script.sh
@@ -5,6 +5,11 @@ echo "\`$par_input\`:"
 echo ""
 
 echo '```'"$par_format"
-cat "$par_input"
-echo ""
+if [ $par_cut ]; then 
+  head "$par_input"
+  echo ... (cut) ...
+else
+  cat "$par_input"
+  echo ""
+fi
 echo '```'
